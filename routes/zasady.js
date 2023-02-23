a05df8d78ff778248c2c55d964846aed6d9d93d2
azukox8 <118135298+azukox8@users.noreply.github.com>
2023-02-23 13:22:53 +0100

    Delete routes directory

--- routes/zasady.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req, res, next) {
-    res.render('zasady', {title: 'Piano4U - Zasady'});
-});
-
-module.exports = router;
